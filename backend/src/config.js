c3bebf5eee9a936e1e92295bec48052d0237bfa6
phellinger <dev@phellinger.com>
2025-01-24 14:24:43 +0100

    better config and .env setup

---
+++ backend/src/config.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  db: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: parseInt(process.env.DB_PORT || '3306', 10),
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  },
+  server: {
+    port: parseInt(process.env.SERVER_PORT || '5193', 10),
+  },
+};
